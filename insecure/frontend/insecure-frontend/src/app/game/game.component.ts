846e293b890bfc08452f675bd8af55785107add9
Michael Werfring <michaelmaximilianw@gmail.com>
2024-04-28 20:46:15 +0200

    Added game board

--- insecure/frontend/insecure-frontend/src/app/game/game.component.ts
+++ insecure/frontend/insecure-frontend/src/app/game/game.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../navbar/navbar.component';
+import { BoardComponent } from "../board/board.component";
 
 @Component({
-  selector: 'app-game',
-  standalone: true,
-  imports: [NavbarComponent],
-  templateUrl: './game.component.html',
-  styleUrl: './game.component.css'
+    selector: 'app-game',
+    standalone: true,
+    templateUrl: './game.component.html',
+    styleUrl: './game.component.css',
+    imports: [NavbarComponent, BoardComponent]
 })
 export class GameComponent {
 
