2dee79ff296831f4c5ea9d3d45fd008a4ab274c2
Michael Werfring <michaelmaximilianw@gmail.com>
2024-04-28 18:10:34 +0200

    added navbar

--- insecure/frontend/insecure-frontend/src/app/game/game.component.ts
+++ insecure/frontend/insecure-frontend/src/app/game/game.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { NavbarComponent } from '../navbar/navbar.component';
 
 @Component({
   selector: 'app-game',
   standalone: true,
-  imports: [],
+  imports: [NavbarComponent],
   templateUrl: './game.component.html',
   styleUrl: './game.component.css'
 })
